f752663663d26d67f30bb6ee58b9b718046afc84
minemaster1329 <minemaster1329@onet.com.pl>
2022-05-22 18:30:51 +0200

    Recovering project structure - Dao, services 2

--- src/main/java/com/example/take_project/models/User.java
+++ src/main/java/com/example/take_project/models/User.java
@@ -1,4 +1,50 @@
 package com.example.take_project.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    private byte[] passwordHash;
+    @Column(unique = true)
+    private byte[] salt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(byte[] passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
 }
--- src/main/java/com/example/take_project/rests/CarREST.java
+++ src/main/java/com/example/take_project/rests/CarREST.java
@@ -1,4 +1,49 @@
 package com.example.take_project.rests;
 
+import com.example.take_project.models.Car;
+import com.example.take_project.services.CarService;
+import com.example.take_project.services.CarServiceInterface;
+import jakarta.ejb.EJB;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.awt.*;
+import java.util.List;
+
+@Path("/car")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CarREST {
+    @EJB
+    CarServiceInterface carService;
+
+    @GET
+    @Path("/getall")
+    public List<Car> getAllCars(){
+        return carService.getAll();
+    }
+
+    @GET
+    @Path("{id}")
+    public Car getById(@PathParam("id") Long id){
+        return carService.getById(id);
+    }
+
+    @POST
+    @Path("/addnew")
+    public void addNewCar(Car car){
+        carService.add(car);
+    }
+
+    @DELETE
+    @Path("/delete/{id}")
+    public void deleteCar(@PathParam("id") Long id){
+        carService.delete(id);
+    }
+
+    @PUT
+    @Path("/update")
+    public void updateCar(Car car){
+        carService.update(car);
+    }
 }
