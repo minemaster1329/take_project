cd0cfaf4ef7903663d7c003528cbea8136b22d11
minemaster1329 <minemaster1329@onet.com.pl>
2022-05-22 19:35:25 +0200

    Fixed Dao and added CORS filter

---
+++ src/main/java/com/example/take_project/CORSFilter.java
@@ -0,0 +1,4 @@
+package com.example.take_project;
+
+public class CORSFilter {
+}
--- src/main/java/com/example/take_project/services/ClientPackageService.java
+++ src/main/java/com/example/take_project/services/ClientPackageService.java
@@ -1,11 +1,11 @@
 package com.example.take_project.services;
 
-import com.example.take_project.daos.ClientPackageDao;
+import com.example.take_project.daos.ClientPackageDaoInterface;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 
 @Stateless
 public class ClientPackageInterface implements ClientPackageServiceInterface{
     @EJB
-    ClientPackageDao clientPackageDao;
+    ClientPackageDaoInterface clientPackageDao;
 }
