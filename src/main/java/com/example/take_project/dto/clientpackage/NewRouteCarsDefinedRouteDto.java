7ab13a418efd4490689bffd59d3a1b52095dcda4
djnick <56382718+djnick@users.noreply.github.com>
2022-06-25 18:34:16 +0200

    Added changes for Route management

---
+++ src/main/java/com/example/take_project/dto/clientpackage/NewRouteCarsDefinedRouteDto.java
@@ -0,0 +1,30 @@
+package com.example.take_project.dto.clientpackage;
+
+import java.util.Date;
+
+public class NewRouteCarsDefinedRouteDto {
+
+    private Long id;
+
+    private Date date;
+
+    private Long carId;
+
+    private Long definedRouteId;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public Date getDate() { return date; }
+
+    public void setDate(Date date) { this.date = date; }
+
+    public Long getCarId() { return carId;  }
+
+    public void setCarId(Long carId) { this.carId = carId; }
+
+    public Long getDefinedRouteId() { return definedRouteId; }
+
+    public void setDefinedRouteId(Long definedRouteId) { this.definedRouteId = definedRouteId; }
+}
--- src/main/java/com/example/take_project/services/RouteService.java
+++ src/main/java/com/example/take_project/services/RouteService.java
@@ -1,17 +1,27 @@
 package com.example.take_project.services;
 
+import com.example.take_project.daos.CarDaoInterface;
+import com.example.take_project.daos.DefinedRouteDaoInterface;
 import com.example.take_project.daos.RouteDaoInterface;
+import com.example.take_project.dto.clientpackage.NewRouteCarsDefinedRouteDto;
+import com.example.take_project.models.Car;
 import com.example.take_project.models.DefinedRoute;
 import com.example.take_project.models.Route;
+import com.example.take_project.otherstuff.exceptions.EntityNotFoundException;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 
+import java.sql.Date;
 import java.util.List;
 
 @Stateless
 public class RouteService implements RouteServiceInterface{
     @EJB
     RouteDaoInterface routeDao;
+    @EJB
+    CarDaoInterface carDaoInterface;
+    @EJB
+    DefinedRouteDaoInterface definedRouteDaoInterface;
 
     @Override
     public Route getById(Long id) {
@@ -26,6 +36,23 @@ public class RouteService implements RouteServiceInterface{
     @Override
     public void addNew(Route cp) {
         routeDao.add(cp);
+    }
+
+    @Override
+    public void addNew(NewRouteCarsDefinedRouteDto newRouteCarsDefinedRouteDto) throws EntityNotFoundException {
+        Route newRoute = new Route();
+
+        Car chosenCar = carDaoInterface.getById(newRouteCarsDefinedRouteDto.getCarId());
+        if (chosenCar == null) throw new EntityNotFoundException(Car.class);
+
+        DefinedRoute chosenDefinedRoute = definedRouteDaoInterface.getById(newRouteCarsDefinedRouteDto.getDefinedRouteId());
+        if (chosenDefinedRoute == null) throw new EntityNotFoundException(DefinedRoute.class);
+
+        newRoute.setDate((Date) newRouteCarsDefinedRouteDto.getDate());
+        newRoute.setRouteType(chosenDefinedRoute);
+        newRoute.setVehicle(chosenCar);
+
+        routeDao.add(newRoute);
     }
 
     @Override
